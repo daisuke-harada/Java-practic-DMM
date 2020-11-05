9f8cad564b47c192296a24626705a3f0372dccb4
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-05 21:44:02 +0900

    [finish] 型の基本から演算まで

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+  public static void main(String[] args) {
+	  long d = 4;
+	  double a = 5;
+	  boolean boolean1 = true;
+	  boolean boolean2 = false;
+	  System.out.println(d);
+	  System.out.println(a);
+	  System.out.println(boolean1);
+	  System.out.println(boolean2);
+	  
+	  Integer i = Integer.valueOf(3);
+	  System.out.println(i);
+	  Float f = Float.valueOf(1.431f);
+	  System.out.println(f);
+	  Character c = Character.valueOf('あ');
+	  System.out.println(c);
+	  int[] j = new int[5];
+	  j[4] = 43;
+	  System.out.println(j[4]);
+  }
+}
