b5dfb4e25886f6e9f7aa89d715eef5d25296eb21
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-05 20:19:46 +0900

    [start] 型と文法

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		
+		String dai;
+	    dai = "harada";
+	    System.out.println(dai);
+		dai = "daisuke";
+		System.out.println(dai);
+		
+		final int LUCKY_NUMBER = 17;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
---
+++ JavaWcp/src/Ready02.java
@@ -0,0 +1,8 @@
+// クラスの始まり
+public class Ready02 {
+	// メソッドの始まり
+	public static void main(String[] args) {
+		// メソッドの処理を記述
+		System.out.println("Hello World!");
+	}
+}
