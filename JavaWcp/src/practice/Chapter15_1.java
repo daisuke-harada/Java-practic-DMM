ae819ce0bca870c999d8d8071bd046bc07f513ac
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-13 22:00:52 +0900

    [add] スレッドのマルチタスク

---
+++ JavaWcp/src/practice/Chapter15_1.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class Chapter15_1 {
+
+	public static void main(String[] args) {
+		int a = 976;
+		int b = 427;
+		System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+	}
+
+	static int gcd(int a, int b) {
+
+		int c = 1;
+        while(c != 0) {
+        	c = a % b;
+        	if (c == 0) {
+        		break;
+        	}else {
+        		a = b;
+            	b = c;
+        	}
+        }
+        return b;
+    }
+
+}
---
+++ JavaWcp/src/practice/ThreadSample.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class ThreadSample {
+
+	public class ThreadSample implements Runnable {
+		int num;
+
+		// コンストラクタ
+	public ThreadSample(int num) {
+        this.num = num;
+    }
+
+		// runメソッドを作成
+		// Chapter15_4と同じようにループごとに1秒間スリープして表示
+		・・・
+	}
+}
