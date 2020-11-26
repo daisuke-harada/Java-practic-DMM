bddf78588cf64b5be039937563c43c3ceeb95e6f
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-26 21:49:27 +0900

    [add]java-silver５章終了

---
+++ JavaWcp/src/practice_silver_5/practice_5_10.java
@@ -0,0 +1,25 @@
+package practice_silver_5;
+
+public class practice_5_10 {
+
+	public static void main(String[] args) {
+		int[] A = {3, 4};
+		int[] B = A;
+		System.out.println(A == B);
+		int[] C = new int[] {3,4};
+		int[] D = C.clone();
+		System.out.println(D == C);
+		for(int i: D) {
+			System.out.println(i);
+		}
+
+		int[][] E = {{2, 3, 4}, {5, 6, 7}};
+		for(int[] i: E) {
+			for(int l: i) {
+			System.out.println(l);
+			}
+		}
+
+	}
+
+}
