acef66db11296945a64b55a7fd7a1c0266496dc9
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-11-28 17:57:11 +0900

    [add] 第6章

---
+++ JavaWcp/src/practice_silver_6/Sample11.java
@@ -0,0 +1,12 @@
+package practice_silver_6;
+
+public class Sample11 {
+	private String value;
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
---
+++ JavaWcp/src/practice_silver_6/practice_6_11.java
@@ -0,0 +1,11 @@
+package practice_silver_6;
+
+public class practice_6_11{
+
+	public static void main(String[] args) {
+		Sample11 s = new Sample11();
+//		戻り値を返さないメソッドに対して戻り値を受け取る変数は宣言できない
+//		String val = s.setValue("hello");
+	}
+
+}
