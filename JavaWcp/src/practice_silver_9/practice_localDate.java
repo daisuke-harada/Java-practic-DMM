9ad2c13d4d4394f732dc95850525665f2acdd5e6
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-03 19:57:45 +0900

    [add]第9章

--- JavaWcp/src/practice_silver_9/practice_localDate.java
+++ JavaWcp/src/practice_silver_9/practice_localDate.java
@@ -9,10 +9,12 @@ public class practice_localDate {
 		// 現在日付を生成
 		LocalDate ld1 = LocalDate.now();
 		System.out.println(ld1);
-
+		
+		// 年月日の数値から生成
 		LocalDate ld2 = LocalDate.of(2018, 9, 1);
 		System.out.println(ld2);
-
+		
+		// 日付文字数から生成
 		LocalDate ld3 = LocalDate.parse("2018-10-01");
 		System.out.println(ld3);
 
