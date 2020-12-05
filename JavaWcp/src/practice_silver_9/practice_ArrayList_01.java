1a5d8c52dbcffeed959a226771923a8b1226eb1d
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-05 19:36:53 +0900

    [add]ArrayList practice

--- JavaWcp/src/practice_silver_9/practice_ArrayList_01.java
+++ JavaWcp/src/practice_silver_9/practice_ArrayList_01.java
@@ -1,5 +1,13 @@
 package practice_silver_9;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class practice_ArrayList_01 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("クリスマス");
+	}
 
 }
