387c3e11ae4cae2e00028d0d0a478798a3da5259
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-07 21:47:25 +0900

    [add] 第8章復習

--- JavaWcp/src/practice_silver_8/practice_8_1.java
+++ JavaWcp/src/practice_silver_8/practice_8_1.java
@@ -2,6 +2,10 @@ package practice_silver_8;
 
 public class practice_8_1 {
 	public static void main(String[] args) {
+		/* ラムダ式は匿名クラスである。インターフェイスはインスタンスを作成することはできないが、
+		 * 匿名クラスとして使用することはできる。
+		 */
+
 		Algorithm algorithm = (String name) -> {
 			System.out.println("hello, " + name);
 		};
