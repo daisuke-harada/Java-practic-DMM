e2a520ae30de3658d4bfd70df43ca70a20a445fa
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-15 22:05:49 +0900

    [add]javaSilver第4章復習

---
+++ JavaWcp/src/practice_silver_04/practice_4_13.java
@@ -0,0 +1,11 @@
+package practice_silver_04;
+
+public class practice_4_13 {
+	public static void main(String[] args) {
+		int num = 10;
+		while (num++ <=10) {
+			num++;
+		}
+		System.out.println(num);
+	}
+}
