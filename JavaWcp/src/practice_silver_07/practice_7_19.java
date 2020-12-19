845b42eb0aabd911057cdc6e7fffc35e41b79cab
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-19 21:20:48 +0900

    [add]第２章

--- JavaWcp/src/practice_silver_07/practice_7_19.java
+++ JavaWcp/src/practice_silver_07/practice_7_19.java
@@ -3,6 +3,8 @@ package practice_silver_07;
 
 public class practice_7_19 {
 	public static void main(String[] args) {
+		var e = new int[] {1, 2, 3};
+		System.out.println(e[0]);
 		Child child = new Child();
 		child.name = "sample";
 		System.out.println(child.getName());
