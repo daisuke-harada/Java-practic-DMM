e8025dbcfd2d2f8fb1788cf433e7a46afd9e6421
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-19 22:29:18 +0900

    [add]インスタンスの挙動の復習

---
+++ JavaWcp/src/practice_silver_06/Animal.java
@@ -0,0 +1,10 @@
+package practice_silver_06;
+
+public class Animal {
+	public String voice;
+	public String foot;
+
+	public String special() {
+		return this.voice;
+	}
+}
---
+++ JavaWcp/src/practice_silver_06/Sample.java
@@ -0,0 +1,10 @@
+package practice_silver_06;
+
+public class Sample {
+	public static void main(String[] args) {
+		Animal animal = new Animal();
+		animal.voice = "グラグラグラ";
+
+		System.out.println(animal.special());
+	}
+}
