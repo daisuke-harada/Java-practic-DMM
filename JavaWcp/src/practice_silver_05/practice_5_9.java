63138d5fa2ae9245641c97fb63fb0ac92199fe78
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-19 22:22:26 +0900

    [add]まとめ問題前の最後の復習

---
+++ JavaWcp/src/practice_silver_05/practice_5_9.java
@@ -0,0 +1,11 @@
+package practice_silver_05;
+
+public class practice_5_9 {
+	public static void main(String[] args) {
+		String[] a = {null, "空", "海"};
+		for(String str: a) {
+			System.out.println(str);
+		}
+	}
+
+}
