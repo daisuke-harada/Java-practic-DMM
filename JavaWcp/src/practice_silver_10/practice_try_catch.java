10141196ed03f633745d33e1bfadd102061359e2
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-20 13:28:11 +0900

    [add] 配列の復習

--- JavaWcp/src/practice_silver_10/practice_try_catch.java
+++ JavaWcp/src/practice_silver_10/practice_try_catch.java
@@ -2,12 +2,18 @@ package practice_silver_10;
 
 public class practice_try_catch {
 	public static void main(String[] args) {
+		String a = "A";
 		try {
 			int[] array = {};
-			array[0] = 10;
+			array[0] = 2;
+			System.out.println(array.length);
 			System.out.println("finish");
 		} catch (Exception e) {
 			System.out.println("error");
+			System.out.println(a);
+		} finally {
+			a = "B";
+			System.out.println(a);
 		}
 	}
 
