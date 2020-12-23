18c3867f000c2b64b3e86e351f6effc48350e38e
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-23 22:40:08 +0900

    [add]まとめ問題

--- JavaWcp/src/practice_silver_06/Sample.java
+++ JavaWcp/src/practice_silver_06/Sample.java
@@ -2,6 +2,10 @@ package practice_silver_06;
 
 public class Sample {
 	public static void main(String[] args) {
+		int def = 5;
+		def++;
+
+		System.out.println(def);
 		Animal animal = new Animal();
 		animal.voice = "グラグラグラ";
 
