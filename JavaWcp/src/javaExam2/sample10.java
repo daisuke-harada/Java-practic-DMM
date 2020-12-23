a01631a29ea79ad6789e9bcb4942210e7b060ed0
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-12-24 08:43:42 +0900

    [add]模試2

---
+++ JavaWcp/src/javaExam2/sample10.java
@@ -0,0 +1,11 @@
+package javaExam2;
+
+import java.util.ArrayList;
+
+public class sample10 {
+	public static void main(String[] args) {
+		// ArrayListはジェネリクスに型の指定がなくても型推論でインスタンスを作成できる。
+		var list = new ArrayList<>();
+		list.add("サスケ");
+	}
+}
